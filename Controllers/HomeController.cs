2e3d60f67fb8ae286ab0dbdd084268a0924aa662
rahadianaldi <rahadianaldi@gmail.com>
2020-10-12 01:12:22 +0700

    First Try

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using GrpcGreeterClient.Models;
+using GrpcGreeterClient;
+
+namespace GrpcGreeterClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            List<Mahasiswa> Res = new List<Mahasiswa>();
+            HomeContext obj = new HomeContext();
+            Res = obj.GetListMahasiswa();
+
+            return View(Res);
+        }
+
+        [HttpPost("GetDetailMahasiswa")]
+        public Mahasiswa DetailMahasiswa([FromQuery] string nim)
+        {
+            Mahasiswa Res = new Mahasiswa();
+            HomeContext obj = new HomeContext();
+            Res = obj.DetailMahasiswa(nim);
+
+            return Res;
+        }
+
+        [HttpPost("InsertMahasiswa")]
+        public string InsertMahasiswa([FromBody] Mahasiswa param)
+        {
+            string Res = "1~Terjadi kesalahan";
+            HomeContext obj = new HomeContext();
+            Res = obj.InsertMahasiswa(param);
+
+            return Res;
+        }
+
+        [HttpPost("EditMahasiswa")]
+        public string EditMahasiswa([FromBody] Mahasiswa param)
+        {
+            string Res = "1~Terjadi kesalahan";
+            HomeContext obj = new HomeContext();
+            Res = obj.EditMahasiswa(param);
+
+            return Res;
+        }
+
+        [HttpPost("DeleteMahasiswa")]
+        public string DeleteMahasiswa([FromQuery] string nim)
+        {
+            string Res = "1~Terjadi kesalahan";
+            HomeContext obj = new HomeContext();
+            Res = obj.DeleteMahasiswa(nim);
+
+            return Res;
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/HomeContext.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GrpcGreeterClient;
+using Grpc.Net.Client;
+
+namespace GrpcGreeterClient.Models
+{
+    public class HomeContext
+    {
+
+        public List<Mahasiswa> GetListMahasiswa()
+        {
+            List<Mahasiswa> Res = new List<Mahasiswa>();
+
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = client.GetListMahasiswa(new empty {});
+
+            foreach (var item in reply.Mahasiswa)
+            {
+                Res.Add(item);
+            }
+            return Res;
+        }
+
+        public Mahasiswa DetailMahasiswa(string nim)
+        {
+            Mahasiswa Res = new Mahasiswa();
+
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+            ID id = new ID();
+            id.Id = nim;
+            var reply = client.DetailMahasiswa(id);
+            Res = reply;
+
+            return Res;
+        }
+
+        public string InsertMahasiswa(Mahasiswa mahasiswa)
+        {
+            string Res = "1~General Error";
+
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+            Mahasiswa mhs = mahasiswa;
+            var reply = client.InsertMahasiswa(mhs);
+            Res = reply.Txt;
+
+            return Res;
+        }
+
+        public string EditMahasiswa(Mahasiswa mahasiswa)
+        {
+            string Res = "1~General Error";
+
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+            Mahasiswa mhs = mahasiswa;
+            var reply = client.EditMahasiswa(mhs);
+            Res = reply.Txt;
+
+            return Res;
+        }
+
+        public string DeleteMahasiswa(string nim)
+        {
+            string Res = "1~General Error";
+
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+            ID id = new ID();
+            id.Id = nim;
+            var reply = client.DeleteMahasiswa(id);
+            Res = reply.Txt;
+
+            return Res;
+        }
+    }
+}
